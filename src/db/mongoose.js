6d781aad28bdb101b9e5012fc6a961b1c6857ec1
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-18 10:57:29 -0600

    Additional data sanitization for tasks type

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -9,19 +9,29 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 export const User = mongoose.model('User', {
   name: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   email: {
     type: String,
     required: true,
+    trim: true,
+    lowercase: true,
     validate(value) {
       if (!validator.isEmail(value)) {
         throw new Error('Email is invalid');
       }
     }
   },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 8
+  },
   age: {
     type: Number,
+    default: 0,
     validate(value) {
       if (value < 0) {
         throw new Error('Age must be a positive number');
@@ -31,6 +41,13 @@ export const User = mongoose.model('User', {
 });
 
 export const Task = mongoose.model('Task', {
-  description: { type: String },
-  completed: { type: Boolean }
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
 });
