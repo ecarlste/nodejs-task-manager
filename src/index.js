ed12e711012707975a61ee0660ef6ae9fc19ef4a
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-18 15:47:59 -0600

    Add PUT methods to update users and tasks

--- src/index.js
+++ src/index.js
@@ -42,6 +42,23 @@ app.get('/users/:id', async (req, res) => {
   }
 });
 
+app.put('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!user) {
+      return res.status(404).send(req.body);
+    }
+
+    return res.send(user);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+});
+
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
 
@@ -72,6 +89,23 @@ app.get('/tasks/:id', async (req, res) => {
     return res.send(task);
   } catch (error) {
     return res.status(500).send(error);
+  }
+});
+
+app.put('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!task) {
+      return res.status(404).send(req.body);
+    }
+
+    return res.send(task);
+  } catch (error) {
+    return res.status(400).send(error);
   }
 });
 
