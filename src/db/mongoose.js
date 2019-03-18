932d2114460bfb755ae8b4153dc10c85a9856746
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-18 10:11:32 -0600

    Adde some required types and validation to User

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -6,8 +7,27 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 });
 
 export const User = mongoose.model('User', {
-  name: { type: String },
-  age: { type: Number }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    }
+  },
+  age: {
+    type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    }
+  }
 });
 
 export const Task = mongoose.model('Task', {
