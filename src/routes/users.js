85f1a05516e78511e255129d87ea790960cf9759
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-18 21:27:50 -0600

    Removed non useful get user by id route

--- src/routes/users.js
+++ src/routes/users.js
@@ -55,19 +55,6 @@ router.get('/me', auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get('/:id', auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    return res.send(user);
-  } catch (error) {
-    return res.status(500).send(error);
-  }
-});
-
 router.put('/:id', auth, async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
