97b500d03204f24d29f2df83939f1f44fba2a9d0
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-19 16:29:47 -0600

    MongoDB URI and express PORT moved to .env

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './db/mongoose';
 import { tasksRouter, usersRouter } from './routes';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use('/tasks', tasksRouter);
