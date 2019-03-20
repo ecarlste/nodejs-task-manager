4da5e208fb491e8653d4e7137dd620fec69a4983
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-20 00:46:41 -0600

    Test for ensuring avatar image is saved

--- tests/user.test.js
+++ tests/user.test.js
@@ -109,3 +109,14 @@ test('Should not delete account for unauthenticated user', async () => {
     .send()
     .expect(401);
 });
+
+test('Should allow user to upload avatar image', async () => {
+  await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .attach('avatar', 'tests/fixtures/profile-pic.jpeg')
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
