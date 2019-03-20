22961eb9bcc28775a10dc5f8c5586a0d16601087
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-19 23:48:22 -0600

    Added tests for /users/login cases

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,9 +2,15 @@ const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/models/user');
 
+const userOne = {
+  name: 'John',
+  email: 'john@test.com',
+  password: 'passtest'
+};
+
 beforeEach(async () => {
-  console.log(process.env.MONGODB_URI);
   await User.deleteMany();
+  await new User(userOne).save();
 });
 
 test('Should signup a new user', async () => {
@@ -16,4 +22,31 @@ test('Should signup a new user', async () => {
       password: 'testpass'
     })
     .expect(201);
+});
+
+test('Should login existing user', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({
+      email: userOne.email,
+      password: userOne.password
+    })
+    .expect(200);
+});
+
+test('Should not login nonexistent user', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({
+      email: 'invalid@nologin.io',
+      password: 'passtest'
+    })
+    .expect(401);
+});
+
+test('Should not login existing user with wrong password', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({ email: userOne.email, password: 'incorrect' })
+    .expect(401);
 });
